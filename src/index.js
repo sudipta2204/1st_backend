3b9cd0193d8c4b53e65e1623269b38c7cc4993ec
sudipta kumar nayak <sudipta18nayak@gmail.com>
2026-07-09 16:11:49 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants"
+
+import express from "express"
+const app= express()
+
+( async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", ()=>{
+        console.error("ERROR:", error)
+        throw error
+       })
+
+       app.listen(process.env.PORT, ()=>{
+        console.log(`app is listening on port ${app.env.PORT}`)
+       })
+    } catch(error){
+        console.error("ERROR:",error)
+        throw err
+    }
+})()
+*/
